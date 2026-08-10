16c03d7e41abf490138d27056b1eb63839da3846
kalaita <kalaitempaddress@gmail.com>
2026-08-10 23:26:58 +0530

    Update and rename tmux.sh to codespace.sh

---
+++ codespace.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update -y &&
+sudo apt install -y tmux &&
+tmux new -s ksi
--- tmux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt update -y &&
-sudo apt install -y tmux &&
-tmux new-session -s ksi "curl -fsSL https://raw.githubusercontent.com/kalaita/Trux/main/codespace.sh | bash"
