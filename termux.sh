074ddaf47ac90f29e0273e570ad92a400e038127
kalaita <kalaitempaddress@gmail.com>
2026-08-10 12:27:53 +0530

    Add files via upload

---
+++ termux.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+termux-change-repo
+
+pkg update -y
+pkg install -y git gh curl
+
+curl -L "https://raw.githubusercontent.com/kalaita/Trux/main/tbashrc" -o "$HOME/.bashrc"
+
+mkdir -p "$HOME/.termux"
+
+curl -L "https://raw.githubusercontent.com/kalaita/Trux/main/colors.properties" -o "$HOME/.termux/colors.properties"
+curl -L "https://raw.githubusercontent.com/kalaita/Trux/main/termux.properties" -o "$HOME/.termux/termux.properties"
+curl -L "https://raw.githubusercontent.com/kalaita/Trux/main/font.ttf" -o "$HOME/.termux/font.ttf"
+
+source "$HOME/.bashrc"
+
+termux-reload-settings
